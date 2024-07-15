80ee7ee84ea68bbda20ebaacb063d95b3f141eb5
Swarup Dhar <swarupdhar123@gmail.com>
2024-07-15 09:40:06 -0400

    Updated config to match latest which key specs

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,13 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
     "folke/which-key.nvim",
-    event = "VimEnter",
+    event = "VeryLazy",
     config = function()
-      require("which-key").setup()
+        local wk = require("which-key")
+        wk.setup {}
 
-      require("which-key").register {
-        ["<leader>f"] = { name = "[F]ile", _ = "which_key_ignore" },
-        ["<leader>q"] = { name = "[Q]uit", _ = "which_key_ignore" },
-        -- ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        -- ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        -- ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        -- ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-        -- ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-        -- ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-      }
-
-      require("which-key").register({
-          ["<leader>h"] = { "Git [H]unk" },
-      }, { mode = "v" })
+        wk.add {
+            { "<leader>f", group = "[F]ile", },
+            { "<leader>s", group = "[S]earch", },
+        }
     end,
 }
